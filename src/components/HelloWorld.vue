e743824fdb3a464d62fdb8976d8de835971705dd
Brendan Slezak <brendan.slezak@ogsystems.com>
2020-05-04 20:31:50 -0400

    basic vue setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="board">
+      <div class="mancala"></div>
+      <div v-for="n in 10" class="pit" :key="n">{{ n }}</div>
+      <div class="mancala"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.mancala {
+  border: 2px;
+}
+.pit {
+  border: 2px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Board msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+// @ is an alias to /src
+import Board from "@/components/Board.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Board
+  }
+};
+</script>
